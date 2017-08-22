376d36652c842c66470a1af3651074bfc1294b3f
AkshayGoelAG <akshaygoel0312@gmail.com>
2017-08-22 15:32:43 +0530

    Commiting necessary code for game

---
+++ app/src/main/java/com/copper/mymario/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.copper.mymario;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, GameActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
